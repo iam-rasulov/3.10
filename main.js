510c502129783d28d7abffafefa0ffbe79371079
iam-rasulov <mirjalolrasulov002@gmail.com>
2022-03-02 20:05:07 +0500

    4th commit

--- main.js
+++ main.js
@@ -8,16 +8,17 @@ var toq = []
 elForm.addEventListener("submit", function(event){
     event.preventDefault();
 
-    if (elInput.value % 2 == 0){
-    elSpan.textContent = elInput.value + " soni juft";
-    juft.push(elInput.value);
-    console.log(juft);
-    elInput.value = "";
+    if (elInput.value <= 0 || isNaN(elInput.value)) {
+        elSpan.textContent =  "0 dan katta raqam kiriting!";
+        elInput.value = "";
+    
     }
-    // else if (elInput.value <= 0 && isNan(elInput.value)) {
-    //     elSpan.textContent =  "0 dan katta raqam kiriting!";
-    //     elInput.value = "";
-    // }
+    else if (elInput.value % 2 == 0){
+        elSpan.textContent = elInput.value + " soni juft";
+        juft.push(elInput.value);
+        console.log(juft);
+        elInput.value = "";
+    }
     else {
     elSpan.textContent = elInput.value + " soni toq";
     toq.push(elInput.value);
